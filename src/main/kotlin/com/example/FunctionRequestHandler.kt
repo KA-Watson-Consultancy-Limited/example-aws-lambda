ccbb7b42dd6e8bf61ee5abcc513a1d2120f461ef
adam rahman <adam@kawc.co.uk>
2023-05-11 17:12:38 +0100

    add transactional annotation

--- src/main/kotlin/com/example/FunctionRequestHandler.kt
+++ src/main/kotlin/com/example/FunctionRequestHandler.kt
@@ -2,12 +2,14 @@ package com.example
 import com.example.aws.ScheduledEvent
 import io.micronaut.function.aws.MicronautRequestHandler
 import jakarta.inject.Inject
+import javax.transaction.Transactional
 
 class FunctionRequestHandler : MicronautRequestHandler<ScheduledEvent, Void?>() {
 
     @Inject
     private lateinit var exampleEntityRepository: ExampleEntityRepository
 
+    @Transactional
     override fun execute(input: ScheduledEvent): Void? {
         println( exampleEntityRepository.findByName("Example") )
         exampleEntityRepository.save("Example")
